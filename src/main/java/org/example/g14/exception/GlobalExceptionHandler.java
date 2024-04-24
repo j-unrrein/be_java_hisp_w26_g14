e1fd7b3a785bb00ed910ad4d5e5b52e113f6b3b0
Julián Unrrein <guillermo.unrrein@mercadolibre.com>
2024-04-24 10:49:58 -0300

    US0007 - Handle error that occurs when any of the endpoint's path variables can't be parsed as integers

--- src/main/java/org/example/g14/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/g14/exception/GlobalExceptionHandler.java
@@ -5,9 +5,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<?> handleNotFoundException(Exception e){
         return new ResponseEntity<>(new ErrorDto(e.getMessage()), HttpStatus.NOT_FOUND);
@@ -18,5 +20,10 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(new ErrorDto(e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
-
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<?> handleMethodArgumentTypeMismatchException(Exception e) {
+        return ResponseEntity.badRequest().body(
+            new ErrorDto("Hubo un error al leer algún parámetro de Path de la petición: " + e.getMessage())
+        );
+    }
 }
