657f6a7c965b24b9f1624e2bc4761e7f68490f07
jsilvanodimarco <silvano.marco@mercadolibre.com>
2024-05-07 17:04:51 -0300

    applies reviewed changes to the tests

--- src/test/java/org/example/g14/service/UserServiceTest.java
+++ src/test/java/org/example/g14/service/UserServiceTest.java
@@ -36,45 +36,36 @@ public class UserServiceTest {
         int userId = 1;
         int userIdToFollow = 20;
 
-        when(userRepository.getById(userId)).thenReturn(Optional.of(
-            User.builder().id(userId).name("John Doe")
-                .idFollowers(new ArrayList<>(){{add(2);add(3);add(4);}})
-                .idFollows(new ArrayList<>(){{add(5);add(6);add(7);}})
-                .build()
-        ));
-        when(userRepository.getById(userIdToFollow)).thenReturn(Optional.of(
-            User.builder().id(userIdToFollow).name("Aria Sanchez")
-                .idFollowers(new ArrayList<>(){{add(18);add(19);}})
-                .idFollows(new ArrayList<>(){{add(21);add(22);}})
-                .build()
-        ));
+        User user = User.builder()
+            .id(userId)
+            .name("John Doe")
+            .idFollowers(new ArrayList<>())
+            .idFollows(new ArrayList<>())
+            .build();
+        User userToFollow = User.builder()
+            .id(userIdToFollow)
+            .name("Aria Sanchez")
+            .idFollowers(new ArrayList<>())
+            .idFollows(new ArrayList<>())
+            .build();
 
-        when(userRepository.getById(5)).thenReturn(
-            Optional.of(User.builder().id(5).name("William Taylor").idFollowers(Arrays.asList(2, 3)).idFollows(Arrays.asList(1, 4)).build())
-        );
-        when(userRepository.getById(6)).thenReturn(
-            Optional.of(User.builder().id(6).name("Olivia Martinez").idFollowers(Arrays.asList(1, 2, 5)).idFollows(Arrays.asList(3, 7)).build())
-        );
-        when(userRepository.getById(7)).thenReturn(
-            Optional.of(User.builder().id(7).name("James Anderson").idFollowers(Arrays.asList(3, 4)).idFollows(Arrays.asList(1, 6)).build())
-        );
+        when(userRepository.getById(userId)).thenReturn(Optional.of(user));
+        when(userRepository.getById(userIdToFollow)).thenReturn(Optional.of(userToFollow));
         when(postRepository.findAllByUser(userIdToFollow)).thenReturn(List.of(new Post()));
 
-        List<UserResponseDto> followedUsers = new ArrayList<>() {{
-            add(new UserResponseDto(5, "William Taylor"));
-            add(new UserResponseDto(6, "Olivia Martinez"));
-            add(new UserResponseDto(7, "James Anderson"));
-            add(new UserResponseDto(20, "Aria Sanchez"));
-        }};
-
-        UserFollowedResponseDto expectedResponse = new UserFollowedResponseDto(userId, "John Doe", followedUsers);
+        UserFollowedResponseDto expectedResponse = new UserFollowedResponseDto(
+            userId,
+            "John Doe",
+            new ArrayList<>(List.of(new UserResponseDto(20, "Aria Sanchez")))
+        );
 
         // Act
         UserFollowedResponseDto response = userService.follow(userId, userIdToFollow);
 
         // Assert
         Assertions.assertEquals(expectedResponse, response);
-        verify(userRepository, times(2)).save(any());
+        verify(userRepository, times(1)).save(user);
+        verify(userRepository, times(1)).save(userToFollow);
     }
 
     @Test
@@ -87,8 +78,8 @@ public class UserServiceTest {
             User.builder()
                 .id(userId)
                 .name("John Doe")
-                .idFollowers(new ArrayList<>(){{add(2);add(3);add(4);}})
-                .idFollows(new ArrayList<>(){{add(5);add(6);add(7);}})
+                .idFollowers(new ArrayList<>())
+                .idFollows(new ArrayList<>())
                 .build()
         ));
         when(userRepository.getById(userIdToFollow)).thenReturn(Optional.empty());
