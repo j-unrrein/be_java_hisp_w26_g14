d1bad0f1460f2633d8d5f66102c8c619ba93f028
joaquin-oldani <joaquin.oldani@mercadolibre.com>
2024-05-07 14:03:17 -0300

    feature t0005 added

---
+++ src/test/java/org/example/g14/service/PostServiceTest.java
@@ -0,0 +1,77 @@
+package org.example.g14.service;
+
+import org.example.g14.dto.ProductDto;
+import org.example.g14.dto.response.PostResponseDto;
+import org.example.g14.exception.OrderInvalidException;
+import org.example.g14.model.User;
+import org.example.g14.repository.PostRepository;
+import org.example.g14.service.IUserServiceInternal;
+import org.example.g14.service.PostService;
+import org.example.g14.utils.PostMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.lang.reflect.Array;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+public class PostServiceTest {
+
+    @Mock
+    IUserServiceInternal iUserServiceInternal;
+
+    @Mock
+    PostRepository repository;
+
+    @InjectMocks
+    PostService service;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        // arrange
+        int sellerId = 1;
+        User user = new User(2, "John Doe", List.of(), List.of(1));
+
+        // act
+        Mockito.when(iUserServiceInternal.searchUserIfExists(user.getId())).thenReturn(user);
+        Mockito.when(repository.findAllByUser(sellerId)).thenReturn(List.of());
+    }
+
+    @Test
+    @DisplayName("US-0009 Verificar que el tipo de ordenamiento por fecha exista. OK")
+    public void getPostsFromFollowedTestOk(){
+        List<PostResponseDto> result_asc = service.getPostsFromFollowed(2, "date_asc");
+        List<PostResponseDto> result_desc = service.getPostsFromFollowed(2, "date_desc");
+
+        // assert
+        Assertions.assertNotNull(result_asc, "El test por fecha ASCENDENTE finalizó correctamente");
+        Assertions.assertNotNull(result_desc, "El test por fecha DESCENDENTE finalizó correctamente");
+    }
+
+    @Test
+    @DisplayName("US-0009 Verificar que el tipo de ordenamiento por fecha exista. BAD REQUEST")
+    public void getPostsFromFollowedTestBadRequest(){
+        // assert
+        Assertions.assertThrows(OrderInvalidException.class, () -> {
+            service.getPostsFromFollowed(2, "asc");
+        });
+
+        Assertions.assertThrows(OrderInvalidException.class, () -> {
+            service.getPostsFromFollowed(2, "desc");
+        });
+    }
+}
