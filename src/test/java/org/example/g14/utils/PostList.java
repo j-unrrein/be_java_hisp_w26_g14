05d6ad112f3a392179d6b50126cb4f34d94b4fde
MarcosBellottiML <marcos.bellotti@mercadolibre.com>
2024-05-07 14:09:01 -0300

    refactor at PostList

--- src/test/java/org/example/g14/utils/PostList.java
+++ src/test/java/org/example/g14/utils/PostList.java
@@ -9,51 +9,43 @@ import java.util.List;
 
 public class PostList {
     public static List<Post> getPostResponse(){
-        Product play = new Product();
-        play.setId(1);
-        play.setName("play station");
-        play.setType("Videojuego");
-        play.setBrand("PS");
-        play.setColor("negro");
-        play.setNotes("");
-
         Post playStation = new Post(
                 LocalDate.of(2024, 05, 05),
                 22,
                 1,
-                play,
+                new Product(1,
+                        "play station",
+                        "videojuego",
+                        "PS", 
+                        "negro",
+                        ""),
                 1
         );
-
-        Product xbox = new Product();
-        xbox.setId(2);
-        xbox.setName("xbox");
-        xbox.setType("Videojuego");
-        xbox.setBrand("PS");
-        xbox.setColor("blanco");
-        xbox.setNotes("");
 
         Post xboxSeries = new Post(
                 LocalDate.of(2024, 05, 01),
                 22,
                 1,
-                xbox,
+                new Product(2,
+                        "xbox",
+                        "videojuego",
+                        "PS",
+                        "blanco",
+                        ""),
                 1
         );
-
-        Product pc = new Product();
-        pc.setId(3);
-        pc.setName("pc");
-        pc.setType("Videojuego");
-        pc.setBrand("PS");
-        pc.setColor("blanco");
-        pc.setNotes("");
 
         Post pcPost = new Post(
                 LocalDate.of(2024, 05, 03),
                 22,
                 1,
-                xbox,
+                new Product(1,
+                        "pc",
+                        "videojuego",
+                        "PS",
+                        "blanco",
+                        ""
+                ),
                 1
         );
 
