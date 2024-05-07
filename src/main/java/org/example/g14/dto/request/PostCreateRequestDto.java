2d5fed2d7f20ae3443dbec2c5be286397ef2ad3b
Julián Unrrein <guillermo.unrrein@mercadolibre.com>
2024-05-07 09:44:35 -0300

    PostCreateRequestDto: Replace @JsonDeserialize with @JsonFormat

--- src/main/java/org/example/g14/dto/request/PostCreateRequestDto.java
+++ src/main/java/org/example/g14/dto/request/PostCreateRequestDto.java
@@ -1,12 +1,11 @@
 package org.example.g14.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.example.g14.dto.ProductDto;
-import org.example.g14.utils.CustomLocalDateDeserializer;
 
 import java.time.LocalDate;
 
@@ -14,11 +13,16 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PostCreateRequestDto {
+
     @JsonProperty("user_id")
     private int idUser;
-    @JsonDeserialize(using = CustomLocalDateDeserializer.class)
+
+    @JsonFormat(pattern = "dd-MM-yyyy")
     private LocalDate date;
+
     private ProductDto product;
+
     private int category;
+
     private double price;
 }
